bcf5746a5729e8215b5f8d3e57c2f91a8a30b574
mkam0012 <50588376+mkam0012@users.noreply.github.com>
2024-10-24 14:39:48 +1100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,29 @@
+// Your web app's Firebase configuration
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAJuORCzYg-gddrxDfX-nONMu-d4Yxyy-Y",
+    authDomain: "apps-taskhooker-com.firebaseapp.com",
+    projectId: "apps-taskhooker-com",
+    storageBucket: "apps-taskhooker-com.appspot.com",
+    messagingSenderId: "485951992785",
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const visitorCountElement = document.getElementById("visitor-count");
+    
+    // Check if the user has visited before
+    const visitorId = localStorage.getItem('visitorId');
+    if (!visitorId) {
+        // New visitor
+        const newVisitorRef = await db.collection('visitors').add({ timestamp: firebase.firestore.FieldValue.serverTimestamp() });
+        localStorage.setItem('visitorId', newVisitorRef.id);
+    }
+
+    // Get the total number of unique visitors
+    const visitorCountSnapshot = await db.collection('visitors').get();
+    visitorCountElement.innerText = visitorCountSnapshot.size;
+});
